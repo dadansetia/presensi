cd4ab4f6f0590bc8b385ed75fd8cb9801743f808
Dadan Setia <dadan.setia@gmail.com>
2023-11-13 11:28:47 +0700

    Update
    update config app

--- www/app/Config/SiteConfig.php
+++ www/app/Config/SiteConfig.php
@@ -6,8 +6,8 @@ class SiteConfig extends BaseConfig
 {
 	public $siteName = "SMA Negeri 1 Lakbok";
 	private $keyEncryption = "VjhgjbZ29Opfdxqm7IvCKL4b384AwR51"; // ganti sesuai kata hati dengan 32 karakter
-	private $apiKey = "";
-	private $secretKey = "";
+	private $apiKey = ""; // Ganti dengan API key SMAN 1 Lakbok
+	private $secretKey = ""; // Ganti dengan secret key SMAN 1 Lakbok
 	private $publicCaptcha = "";
 	private $privateCaptcha = "";
 	private $publicTurnstile = "";
