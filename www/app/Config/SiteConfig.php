e17ed79491d6878e7dcd3b0064b5884242a4cbbc
Dadan Setia <dadan.setia@gmail.com>
2024-01-31 10:59:30 +0700

    Update
    update site config

--- www/app/Config/SiteConfig.php
+++ www/app/Config/SiteConfig.php
@@ -5,7 +5,7 @@ use CodeIgniter\Config\BaseConfig;
 class SiteConfig extends BaseConfig 
 {
 	public $siteName = "SMA Negeri 1 Lakbok";
-	private $keyEncryption = "AwRvCKLdxqm7Ib384451VjhgjbZ29Opf"; // ganti sesuai kata hati dengan 32 karakter
+	private $keyEncryption = "AwRvCKL7Ib384451VjdxqmhgjbZ29Opf"; // ganti sesuai kata hati dengan 32 karakter
 	private $apiKey = ""; // Ganti dengan API key SMAN 1 Lakbok
 	private $secretKey = ""; // Ganti dengan secret key SMAN 1 Lakbok
 	private $publicCaptcha = "";
